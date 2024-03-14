392d3c01c7387a76e4b3e52f8db9cbbdef21d430
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-14 21:32:38 +0300

    refactoring of DIfferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -15,6 +15,12 @@ public class DifferTest {
                 .toAbsolutePath().normalize();
     }
     @Test
+    void testDifferGenerate1() throws Exception {
+        String result1 = Differ.generate("file1.json", "file2.json", "stylish");
+        Path expected1 = DifferTest.getPath("jsonStylish.txt");
+        String expectedResults = Files.readString(expected1);
+        assertEquals(result1, expectedResults);
+    }
     void testDifferGenerate2() throws Exception {
         String result1 = Differ.generate("file1.yml", "file2.yml", "stylish");
         Path expected1 = DifferTest.getPath("ymlStylish.txt");
