9fec02fc720e71323ec753397c96377e6430d0ad
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-14 20:59:05 +0300

    refactoring

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DifferTest {
+
+    private static Path getPath(String fileName) {
+        return Paths.get("src", "test", "resources", fileName)
+                .toAbsolutePath().normalize();
+    }
+    @Test
+    void testDifferGenerate2() throws Exception {
+        String result1 = Differ.generate("file1.yml", "file2.yml", "stylish");
+        Path expected1 = DifferTest.getPath("ymlStylish.txt");
+        String expectedResults = Files.readString(expected1);
+        assertEquals(result1, expectedResults);
+    }
+}
