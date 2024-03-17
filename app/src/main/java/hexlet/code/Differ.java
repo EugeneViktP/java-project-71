0711ffe0e69d7454bd11216bd6374b4b584cdd5c
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-17 21:42:55 +0300

    cleared comments for Differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,13 +1,6 @@
 package hexlet.code;
 
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import java.io.IOException;
-
-//import java.util.LinkedHashMap;
 import java.util.Map;
-//import java.util.Set;
-//import java.util.TreeSet;
-//import java.util.Objects;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
@@ -23,24 +16,4 @@ public class Differ {
         var data = DiffBuilder.compareData(buffer1, buffer2);
         return Formatter.format(data, "stylish");
     }
-    // DiffBuilder
-//    public static Map<String, StatusValuesDB> diffBuilder(Map<String, Object> data1, Map<String, Object> data2) {
-//        Map<String, StatusValuesDB> result = new LinkedHashMap<>();
-//        Set<String> union = new TreeSet<>();
-//        union.addAll(data1.keySet());
-//        union.addAll(data2.keySet());
-//        for (String key : union) {
-//            if (!data1.containsKey(key)) {
-//                result.put(key, new StatusValuesDB("added", data2.get(key)));
-//            } else if (!data2.containsKey(key)) {
-//                result.put(key, new StatusValuesDB("deleted", data1.get(key)));
-////                result.put(key, new StatusValuesDB("deleted", data2.get(key)));
-//            } else if (Objects.equals(data1.get(key), data2.get(key))) {
-//                result.put(key, new StatusValuesDB("unchanged", data1.get(key)));
-//            } else {
-//                result.put(key, new StatusValuesDB("changed", data1.get(key), data2.get(key)));
-//            }
-//        }
-//        return result;
-//    }
 }
