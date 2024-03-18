709f6b7863ca866c69b0e25cc5af477a5753dc38
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-18 21:13:22 +0300

    updated Diffbuilder

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -11,25 +11,25 @@ import java.util.ArrayList;
 
 
 public class DiffBuilder {
-    public static Map<String, StatusValuesDB> compareData(Map<String, Object> data1, Map<String, Object> data2) {
-        Map<String, StatusValuesDB> result = new LinkedHashMap<>();
-        Set<String> union = new TreeSet<>();
-        union.addAll(data1.keySet());
-        union.addAll(data2.keySet());
-        for (String key : union) {
-            if (!data1.containsKey(key)) {
-                result.put(key, new StatusValuesDB("added", data2.get(key)));
-            } else if (!data2.containsKey(key)) {
-                result.put(key, new StatusValuesDB("deleted", data1.get(key)));
-//                result.put(key, new StatusValuesDB("deleted", data2.get(key)));
-            } else if (Objects.equals(data1.get(key), data2.get(key))) {
-                result.put(key, new StatusValuesDB("unchanged", data1.get(key)));
-            } else {
-                result.put(key, new StatusValuesDB("changed", data1.get(key), data2.get(key)));
-            }
-        }
-        return result;
-    }
+//    public static Map<String, StatusValuesDB> compareData(Map<String, Object> data1, Map<String, Object> data2) {
+//        Map<String, StatusValuesDB> result = new LinkedHashMap<>();
+//        Set<String> union = new TreeSet<>();
+//        union.addAll(data1.keySet());
+//        union.addAll(data2.keySet());
+//        for (String key : union) {
+//            if (!data1.containsKey(key)) {
+//                result.put(key, new StatusValuesDB("added", data2.get(key)));
+//            } else if (!data2.containsKey(key)) {
+//                result.put(key, new StatusValuesDB("deleted", data1.get(key)));
+////                result.put(key, new StatusValuesDB("deleted", data2.get(key)));
+//            } else if (Objects.equals(data1.get(key), data2.get(key))) {
+//                result.put(key, new StatusValuesDB("unchanged", data1.get(key)));
+//            } else {
+//                result.put(key, new StatusValuesDB("changed", data1.get(key), data2.get(key)));
+//            }
+//        }
+//        return result;
+//    }
 
 //    New way of creating DataBase for Formatting through List<Map<String, Object>>
 
