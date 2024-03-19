a75e3fb5ab569a19f8f373c164088c288d236040
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-19 22:16:30 +0300

    refactoring Formatter

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -8,14 +8,6 @@ import java.util.Map;
 
 public class Json {
     public static String formatter(List<Map<String, Object>> data) throws JsonProcessingException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        String json = "";
-//        try {
-//            json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return json;
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
     }
