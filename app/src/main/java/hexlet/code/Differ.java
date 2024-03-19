a753caf653bfb1934914ef706d19513b68e0d157
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-19 21:55:28 +0300

    refactoring Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -15,14 +15,6 @@ public class Differ {
     }
 
     public static String generate(String filepath1, String filepath2) throws Exception {
-//        String textOne = Utils.createInToParse(filepath1);
-//        String textTwo = Utils.createInToParse(filepath2);
-//        String firstFileExtension = Utils.getFileExtension(filepath1);
-//        String secondFileExtension = Utils.getFileExtension(filepath2);
-//        Map<String, Object> buffer1 = Parser.parse(textOne, firstFileExtension);
-//        Map<String, Object> buffer2 = Parser.parse(textTwo, secondFileExtension);
-//        var data = DiffBuilder.createData(buffer1, buffer2);
-//        return Formatter.format(data, "stylish");
         return  generate(filepath1, filepath2, "stylish");
     }
 }
