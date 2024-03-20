32511c6394da04d694dc119b068fa248ece6e5ef
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-20 20:11:40 +0300

    refactoring DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -18,8 +18,6 @@ public final class DifferTest {
     private static String expectedResults2;
     private static String expectedResults3;
 
-
-
     private static Path getPath(String fileName) {
         return Paths.get("src", "test", "resources", "fixtures", fileName)
                 .toAbsolutePath().normalize();
@@ -32,14 +30,11 @@ public final class DifferTest {
         expectedResults1 = Files.readString(expected1);
         expectedResults2 = Files.readString(expected2);
         expectedResults3 = Files.readString(expected3);
-
     }
 
     @ParameterizedTest
     @CsvSource({"file1.json, file2.json", "file1.yml, file2.yml"})
     void testDifferGenerate1(String firstFile, String secondFile) throws Exception {
-//        String file1 = getPath("file1.json").toString();
-//        String file2 = getPath("file2.json").toString();
         String file1 = getPath(firstFile).toString();
         String file2 = getPath(secondFile).toString();
         String result1 = Differ.generate(file1, file2);
@@ -53,23 +48,5 @@ public final class DifferTest {
 
         String result4 = Differ.generate(file1, file2, "json");
         assertEquals(expectedResults3, result4);
-
     }
-
-//    @Test
-//    void testDifferGenerate2() throws Exception {
-//        String file3 = getPath("file1.yml").toString();
-//        String file4 = getPath("file2.yml").toString();
-//        String result1 = Differ.generate(file3, file4);
-//        assertEquals(expectedResults1, result1);
-//
-//        String result2 = Differ.generate(file3, file4, "stylish");
-//        assertEquals(expectedResults1, result2);
-//
-//        String result3 = Differ.generate(file3, file4, "plain");
-//        assertEquals(expectedResults2, result3);
-//
-//        String result4 = Differ.generate(file3, file4, "json");
-//        assertEquals(expectedResults3, result4);
-//    }
 }
